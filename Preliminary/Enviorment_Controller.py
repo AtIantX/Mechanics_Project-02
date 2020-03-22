fae167459bc0a849b03ee4f8f53066bdb95fd9c4
AlantX <john.zohrabyan@gnmail.com>
2020-03-22 16:15:14 +0400

    Main Structure and controllers added

---
+++ Preliminary/Enviorment_Controller.py
@@ -0,0 +1,21 @@
+from random import randrange
+
+
+class Enviorment:
+
+    def __init__(self):
+        self.mass_range = (0, 10)
+        self.friction_range = (0, 0.5)
+        self.external_force_range = (-300, 300)
+        self.g = 10
+
+    def get_variables_defined_random(self):
+        mass_1 = randrange(*self.mass_range)
+        friction_1 = randrange(*self.friction_range)
+        mass_2 = randrange(*self.mass_range)
+        friction_2 = randrange(*self.friction_range)
+        mass_3 = randrange(*self.mass_range)
+        friction_3 = randrange(*self.friction_range)
+        external_force = randrange(*self.external_force_range)
+        return {(mass_1, friction_1), (mass_2, friction_2), (mass_3, friction_3), external_force, self.g}
+
