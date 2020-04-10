b5307cbaebc864e22b8494b7e35e76ec1c53d193
AlantX <john.zohrabyan@gnmail.com>
2020-04-11 01:41:48 +0400

    Added Environment and Object Models also with Variable Controller

---
+++ Final/ObjectModel.py
@@ -0,0 +1,11 @@
+# Description of physical object in the problem
+
+
+class PhysicalObject:
+
+    def __init__(self):
+        self.weight = 0
+        self.friction = 0
+        self.pos = (0, 0)
+
+
---
+++ Final/Variable_Controller.py
@@ -0,0 +1,23 @@
+from random import randrange
+
+
+class VariableController:
+
+    g = 10
+
+    def __init__(self):
+        self.mass_range = (0, 10)
+        self.friction_range = (0, 0.5)
+        self.external_force_range = (-300, 300)
+
+    def get_random_mass(self):
+        mass = randrange(*self.mass_range)
+        return mass
+
+    def get_random_friction(self):
+        friction = randrange(*self.friction_range)
+        return friction
+
+    def get_random_external_force(self):
+        external_force = randrange(*self.external_force_range)
+        return external_force
